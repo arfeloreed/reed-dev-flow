dc70e2b5fee38f9c2d3518f64ce1403f50667236
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2024-11-10 10:12:11 +0800

    added assets and edited metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
 import React from "react";
 
 import "@styles/globals.css";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
+
 export const metadata: Metadata = {
   title: "Reed DevFlow",
-  description: "A better version of StackOverFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-purple-500">Hello World</h1>
+      <h1 className="text-4xl font-bold text-purple-500">
+        Hello World - inter
+      </h1>
+      <h1 className="font-space-grotesk text-4xl font-bold text-purple-500">
+        Hello World - space grotesk
+      </h1>
     </div>
   );
 };
