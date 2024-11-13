6bac9d9cb7a06ebae79978fa4a28eba893a99b7b
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2024-11-13 12:08:42 +0800

    added toaster, implemented github and google auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-const Home = () => {
+import { signOut, auth } from "@auth";
+import { Button } from "@components/ui/button";
+import ROUTES from "@constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <div>
       <h1 className="text-4xl font-bold text-purple-500">
@@ -7,6 +14,16 @@ const Home = () => {
       <p className="font-space-grotesk text-4xl font-bold text-purple-500">
         Hello World - space grotesk
       </p>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
     </div>
   );
 };
