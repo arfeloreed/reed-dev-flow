8701429a3875fba34db021afdb82e4baaa213a18
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2025-02-03 17:21:54 +0800

    Implement Edit and Get Server Actions and Implemention

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import React from "react";
 
-import Metric from "@/components/Metric";
 import ROUTES from "@/constants/routes";
 import { getTimeStamp } from "@/lib/utils";
 
 import TagCard from "./TagCard";
+import Metric from "../Metric";
 
 interface Props {
   question: Question;
